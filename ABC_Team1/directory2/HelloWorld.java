4c5e2cd4eb47b6a86e14686c1193353e34d3338f
Nalini Kanta Pattanayak <nalinikanta.pattanayak@oracle.com>
2024-05-09 21:13:51 +0530

    Added display()

--- ABC_Team1/directory2/HelloWorld.java
+++ ABC_Team1/directory2/HelloWorld.java
@@ -4,7 +4,10 @@ public static void main (String[] args) {
 System.out.println("Say HelloWorld");
 System.out.println("This is a new line added");
 System.out.println("Changed 1");
+display();
 }
-
+public static void display(){
+System.out.println("Display Hello World");
+}
 }
 
